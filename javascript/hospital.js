2446c7d4851fc8542da3ca4bfe259e23fd6dcd9a
dhivya051998 <dhivyap18598@gmail.com>
2021-05-12 12:25:05 +0530

    javascript file add

---
+++ javascript/hospital.js
@@ -0,0 +1,137 @@
+var hospital ={
+    
+    hospital_name:"zzzz",
+    address:"200, ambai",
+
+     doctor:{
+        name:"qqq",
+        ID:32,
+        gender:"female",
+        qualification:"MBBS",
+        age:34,
+        experience:"visiting " || "permanent" || "trainee" || "employee"
+        
+     },
+
+        appoinment:{
+            patient_name:"fff",
+            detail:"vvv",
+            gender:"male",
+            date:"20-04-2020",
+            Time:9.00,
+            tokemno:30
+           
+        },
+
+            nurse:{
+                name:"mmm",
+                id:"5674",
+                gender:"female",
+                age:38,
+                qualification:"BSC",
+                experience:"2, years"
+            },
+
+                patient:{
+
+                    inpatient:{
+                    name:"hhhh",
+                    tokenno:"45",
+                    age:40,
+                    gender:"male",
+                    address:"300, kadayam",
+                    date_of_birth:"20-05-1980",
+                    bloodgroup:"a",
+                    disease:"fever",
+                    consulting:"www",
+                    date_of_admission:"23-10-2020",
+                    date_of_discharge:"24-11-2020"
+                    },
+
+                    outpatient:{
+                        inpatient:{
+                            name:"hhhh",
+                            tokenno:"45",
+                            age:40,
+                            gender:"male",
+                            address:"300, kadayam",
+                            date_of_birth:"20-05-1980",
+                            bloodgroup:"a",
+                            disease:"fever",
+                            consulting:"www",
+                            
+                            
+                    }
+                },
+
+                    room:{
+                        roomno:23,
+                        charges:300,
+                        roomtype:"singleroom, sharingroom",
+                        patientname:"yyy",
+                        address:"02,tenkasi",
+                        available:20
+                    },
+
+                        medicine:{
+                            name:"ooo",
+                            id:20034,
+                            rate:390,
+                            quantity:2
+                        },
+                        
+                            medicalstore:{
+                                name:"sss",
+                                address:"45,kadayam"
+                            },
+                                lab:{
+                                    name:"hhhh",
+                                    tokenno:"45",
+                                    age:40,
+                                    gender:"male",
+                                    address:"300, kadayam",
+                                    date_of_birth:"20-05-1980",
+                                    bloodgroup:"a",
+                                    disease:"fever",
+                                    testname:"blood",
+                                    id:43,
+                                    amount:800
+                                },
+                                    stock:{
+                                        name:"ttt",
+                                        productid:"rtyh",
+                                        quantity:50
+                                        
+                                    },
+                                        
+                                        bill:{
+                                            
+                                            patient_name:"ppp",
+                                            address:"300, kadayam",
+                                            patient_type:"fever",
+                                            billnumber:56,
+                                            noofdays:7,
+                                            charges_type:"lab charges,room charges,nursing charges,medicine charges,doctor charges"
+                                            
+
+                                        },
+                                        supplier:{
+                                            name:"xxx",
+                                            address:"04 tenkasi",
+                                            product_item:"nnnn",
+                                            quantity:300,
+                                            rate:500,
+                                            discount:10
+
+                                        },
+                                        account:{
+                                            date:"31-03-2020",
+                                            heading:"www",
+                                            amount:30000,
+                                            openingstock:200,
+                                            closingstock:50,
+                                        
+                                        }
+   
+                                    }
+}
---
+++ javascript/library.js
@@ -0,0 +1,89 @@
+var library={
+    name:"abc",
+    address: "kadayam",
+
+    librarian:{
+        employee:{
+            name:"a",
+            id:75,
+            address:" 309, tenkasi",
+            email_id:"aaa@gmail.com",
+            age:20,
+            gender:female,
+            phone_no:9867542354,
+            qualification:"b.com"
+        },
+
+        temporary:{
+        name:"b",
+        id:98,
+        address:" 250, kadayam",
+        emailid:"bbb@gmail.com",
+        age:34,
+        gender:male,
+        phone_no:7967352780,
+        qualification:"B.Sc"
+       
+        
+        }
+
+    },
+      student:{
+         name:"xxx",
+         id:54,
+         addrress:"34, chennai",
+         gender:"female",
+         email_id:"xxx@gmail.com",
+         department:"commerce",
+         mobile_number:7854632790,
+         create_date:"24-10-2019",
+         renewal_date:"25-12-2019"
+
+      },
+       librarycard:{
+           name:"ccc",
+           rollno:67,
+           department:"commerce",
+           cardnumber:900,
+           book_name:"mmm",
+           no_of_books:2,
+           expiray_date:"15-03-2019"
+
+       },
+       book:{
+           name:"aaa",
+           title:"bbb",
+           book_id:28970,
+           author_name:"zzz",
+           due_date:"03-09-2020",
+           category:"text,reference,novels",
+           others:"questionspaper,news paper, magzine"
+              
+           
+       },
+          member:{
+              name:"ddd",
+              id:70,
+              date:"29-04-2020",
+              no_of_bookissued:2,
+              max_numberlimited:3,
+              address:"53,mumbai",
+              category:"staff, student"
+
+          },
+          reader:{
+              name:"yyy",
+              id:78,
+              roll_no:34,
+              time:3.00,
+              bookname:"xxx"
+
+          },
+          stock:{
+              book_name:"sss",
+              author_name:"www",
+              category:"issue,return,renewal"
+
+          }
+
+}
