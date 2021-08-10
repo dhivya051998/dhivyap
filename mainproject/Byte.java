5639b330e0e11b6eb9346142ef675751d2058601
dhivya051998 <82437897+dhivya051998@users.noreply.github.com>
2021-08-10 21:59:11 +0530

    add automation testing

---
+++ mainproject/Byte.java
@@ -0,0 +1,37 @@
+package mainproject;
+
+public class Byte {
+	  public static void main(String[] args) {
+		  
+		  byte num = 90;
+		  System.out.println(num);
+		   
+		  short a = -9000;
+		  System.out.println(a);
+		  
+		  int name = 200000;
+		  System.out.println(name);
+		  
+		  long demo = 30000000000L;
+		  System.out.println(demo);
+		  
+		  float box = 98.75383490f;
+		  System.out.println(box);
+		  
+		  double myNum = 6.657890876d;
+		  System.out.println(myNum);
+		  
+		  boolean isFun = true;
+		  boolean isFish = false;
+		  System.out.println(true);
+		  System.out.println(false);
+		  
+		  String greeting = "Hello";
+		  System.out.println(greeting);
+		  
+		  
+		  
+		  
+		  
+	  }
+}
