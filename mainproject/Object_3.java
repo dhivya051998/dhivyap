f70590ec3d1e58adea6e28e77b5eed72006319d0
dhivya051998 <82437897+dhivya051998@users.noreply.github.com>
2021-08-11 13:04:37 +0530

    add automation testing

---
+++ mainproject/Object_3.java
@@ -0,0 +1,23 @@
+package mainproject;
+
+public class Object_3 {
+	int id;
+	String name;
+	 public static void main(String[] args) {
+		 
+		 Object_3 student_1 = new Object_3();
+		 Object_3 student_2 = new Object_3();
+		 
+		 student_1.id = 105;
+		 student_1.name = "aaa";
+		 
+		 student_2.id = 160; 
+		 student_2.name = "bbb"; 
+		 
+		 System.out.println(student_1.id+ "" +student_1.name);
+		 System.out.println(student_2.id+ "" +student_2.name);
+		 
+		 
+	 }
+
+}
