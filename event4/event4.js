f0ba7fccc47487eccf7d23eb18673b35a1d59eb1
dhivya051998 <82437897+dhivya051998@users.noreply.github.com>
2022-02-16 13:16:32 +0530

    event project add

--- event4/event4.js
+++ event4/event4.js
@@ -1,44 +1,71 @@
-
-setInterval(display, 500);
 var increase = 1;
 var value = [];
+var newDiv = [];
 var a = 1;
-function display(){
+var displayTime = 0;
+function move() {
+    var moveDiv = document.querySelector(".box1");
     var x = document.querySelector(".box2");
-    if(a<=600){
-        x.style.left = (500+a) + "px";
+    var inputValue = document.querySelector("#time").value;
+    var boxDiv = document.querySelector(".box");
+    var divWidth = boxDiv.getBoundingClientRect().width;
+    console.log(divWidth);
+    var z = inputValue/divWidth;
+    displayTime +=z;
+    document.querySelector(".box1").innerText = displayTime.toFixed(2);
+    if (a <= divWidth){  
+        x.style.left = (500 + a) + "px";
+        moveDiv.style.left = (500+a) + "px";
         a++;
     }
-    for(i=0; i<value.length; i++){
-        if((500+a) == value[i]){
-            document.querySelector(".createDiv2").style.backgroundColor = "red";
-            // alert("same position");
+    for (i = 0; i < value.length; i++) {
+        if ( value[i] == (500+a)) {
+           newDiv[i].style.backgroundColor = "#78c9c8";
+         
         }
     }
-} 
-function createBox(e){
+}
+function display(){
+    // var moveDiv = document.querySelector(".box1");
+    // var x = document.querySelector(".box2");
+    var boxDiv = document.querySelector(".box");
+    var divWidth = boxDiv.getBoundingClientRect().width;
+    var inputValue = document.querySelector("#time").value;
+    var z = inputValue/divWidth;
+    setInterval(() => {
+        if (a <= divWidth){
+            move(); 
+        }  
+     },z*1000);
+}
+function createBox(e) {
     var x = document.querySelector(".createDiv1");
     var y = x.cloneNode(true);
     y.removeAttribute("id");
-    var create = document.querySelector(".box");
+    var create = document.querySelector(".container");
     create.appendChild(y);
     y.style.left = e.x + "px";
     value.push(e.x);
-    console.log(value);
+    
 
- 
     var a = document.querySelector(".createDiv2");
     var b = a.cloneNode(true);
     b.removeAttribute("id");
-    var create = document.querySelector(".box");
+    var create = document.querySelector(".container");
     create.appendChild(b);
-    b.style.left = e.x+ "px"; 
+    b.style.left = e.x + "px";
+    var inputValue = document.querySelector("#time").value;
     var x = e.x - 500;
-    var percentage = x/600*100;
-    var time = 60/100*percentage;
-    b.innerHTML = "0" +":" + Math.floor(time)+ "<br>" +"Task- "+increase;
-    increase++;
-   
-    
-}  
+    newDiv.push(b);
+    console.log(newDiv);
+    var boxDiv = document.querySelector(".box");
+    var divWidth = boxDiv.getBoundingClientRect().width;
+    // var percentage = x / 600 * 100;
+    // var time = 60 / 100 * percentage;
+    // b.innerHTML = "0" + ":" + Math.floor(time) + "<br>" + "Task- " + increase;
+    // increase++;
+    var position = inputValue/divWidth*x;
+    console.log(position);
+    b.innerHTML = position.toFixed(2);
+}
 
