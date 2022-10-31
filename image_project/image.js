546562f6d8700123c2d74fe1bd2ca9ccafc33c3f
dhivya051998 <82437897+dhivya051998@users.noreply.github.com>
2022-10-31 12:29:12 +0530

    Add files via upload

---
+++ image_project/image.js
@@ -0,0 +1,10 @@
+function showImage(img_class){
+    document.querySelector(".allDiv").style.opacity = "0.2";
+    document.querySelector(".scale1").style.display = "flex";
+    var comment_fields = document.querySelector(".scale1");
+    comment_fields.classList.add(img_class);
+} 
+function closeDiv() {
+    document.querySelector(".scale1").style.display = "none";
+    document.querySelector(".allDiv").style.opacity = "";
+}
