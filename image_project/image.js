b7a14f7922b21cc61738ac3ed8371b67ef2723fc
dhivya051998 <82437897+dhivya051998@users.noreply.github.com>
2022-11-01 10:43:46 +0530

    Update image.js

--- image_project/image.js
+++ image_project/image.js
@@ -1,10 +1,12 @@
 function showImage(img_class){
+    document.querySelector(".container").style.pointerEvents="none";
     document.querySelector(".allDiv").style.opacity = "0.2";
     document.querySelector(".scale1").style.display = "flex";
     var comment_fields = document.querySelector(".scale1");
     comment_fields.classList.add(img_class);
 } 
 function closeDiv() {
+    document.querySelector(".container").style.pointerEvents="auto";
     document.querySelector(".scale1").style.display = "none";
     document.querySelector(".allDiv").style.opacity = "";
 }
